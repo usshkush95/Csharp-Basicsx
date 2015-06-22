0d8248a01f92439ec0aa9e7cce971cbd7b3680ba
unknown <blue14@RM02-14.fasfg.local>
2015-06-22 13:53:29 +0100

    Simple Calculator has been added

---
+++ Lesson3/Calculator/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calculator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //string hello;
+            //hello = "hello world";
+            //MessageBox.Show(hello);
+            //MessageBox.Show("hello world");
+            int firstTextBox = 0;
+            int secondTextBox = 0;
+            int result = 0;
+            
+            firstTextBox = int.Parse(textBox1.Text);
+            secondTextBox = int.Parse(textBox2.Text); 
+            
+            result = firstTextBox + secondTextBox; 
+            label3.Text = result.ToString();
+
+        }
+    }
+}
